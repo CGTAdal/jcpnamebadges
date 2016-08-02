f5023415b291fcbd2696dbc5200e6cacc265f621
Aman ali <aman.ali@cgt.co.in>
2016-08-02 17:53:38 +0530

    Excel Export Rebuilt

--- application/modules/order/controllers/admin/order.php
+++ application/modules/order/controllers/admin/order.php
@@ -1,4 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+include(APPPATH.'libraries/PHPExcel/PHPExcel.php');
 class Order extends MX_Controller {
 	
 	var $order_table = 'orders';
@@ -66,17 +67,10 @@ class Order extends MX_Controller {
 		
 		$orders = $this->order_model->getOrderList($filter);
 		$data['orders']	= $orders;
-		$view 	= $this->load->view('order/admin/list_excel',$data);
-		
-		$file	 = "JCP-Output.xls";
-		$content = "";
-		header("Content-type: application/excel");
-		header("Content-Disposition: attachment; filename=$file");
-		header("Pragma: no-cache");
-		header("Expires: 0");
-		echo $view;
+		$this->excelExport($data['orders']);
+		exit;
 	}
-	
+
 	function listOrders() {
 		if($this->input->post('sort_type')) {
 			// set order_by
@@ -267,4 +261,201 @@ class Order extends MX_Controller {
 			redirect('admin/order/listorders');
 		}  
 	}
+
+	function excelExport($orders)
+	{
+		// echo '<pre>';print_r($orders);
+		$objPHPExcel = new PHPExcel();		
+
+		$objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A1', 'Order Date')
+            ->setCellValue('B1', 'Order #')
+            ->setCellValue('C1', 'Unit #')
+            ->setCellValue('D1', 'Ship Date')
+            ->setCellValue('E1', 'AOR')
+            ->setCellValue('F1', 'JCPenney Name Badge')
+            ->setCellValue('G1', 'Generic (No Name)')
+            ->setCellValue('H1', 'Optical')
+            ->setCellValue('I1', 'Salon')
+            ->setCellValue('J1', 'In-Home Window Treatments')
+            ->setCellValue('K1', '5-Pack Magnets')
+            ->setCellValue('L1', '5-Pack Pins') 
+            ->setCellValue('M1', '5-Pack Adhesive Charm Extender')
+            ->setCellValue('N1', '5-Pack Salon Magnetic Charm Bar');
+
+		$row_number  = 'O';            
+		for($i=0;$i<=65;){
+        	$objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.'1',(($i==0) ? 1 : $i).' Y 5-Pack');				
+			$i= $i+5; 
+			$row_number++;
+		}   
+
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.'1', 'Badge Cost'); 
+        $row_number++;
+        
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.'1', 'Handling');
+        $row_number++;
+        
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.'1', 'Total');		
+        $row_number++;	
+
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.'1', 'Net Cost'); 
+        $row_number++;
+        
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.'1', 'Tracking Number');		
+        $row_number++;
+
+		$styleArray = array(
+	    'font'  => array(
+	        'size'  => 10,
+	        'name'  => 'Liberation Serif',
+	    ),
+			'alignment' => array(
+	            'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+	        )
+	    );
+
+		$objPHPExcel->getActiveSheet()->getStyle('1:1')->getFont()->setBold(true);  
+		$objPHPExcel->getActiveSheet()->getStyle('1:1')->applyFromArray($styleArray);		                                                      
+
+		$objPHPExcel->getActiveSheet()->setTitle('JCP-Output');	          
+
+		if(count($orders)>0) {
+		$i=0;
+		$row_count=2;
+			foreach($orders as $order) {
+				$badgeshipping	= @unserialize($order->order_shipping); 
+
+				$badges	= @unserialize($order->order_items); 
+				$badgesdatalist = (isset($badges['badges']))?$badges['badges']:Null;
+				$badgesData = "";
+				$k=0;
+				$badgesList = array();
+				if($badgesdatalist){
+					asort($badgesdatalist);
+					foreach($badgesdatalist as $key => $value ){ 
+						if($k==0){ $badgesData = @$value['badge_Id']; }							
+						if(!isset($badgesList[@$value['badge_Id']]['count'])){ $badgesList[@$value['badge_Id']]['count'] = 0;}
+						if($badgesData==@$value['badge_Id']){
+							$badgesList[@$value['badge_Id']]['bid'] = @$value['badge_Id'];	
+							$badgesList[@$value['badge_Id']]['count'] +=1; 
+							$badgesList[@$value['badge_Id']]['name']=$value['style']; 
+						}else{
+							$badgesData = @$value['badge_Id']; 
+							$badgesList[@$value['badge_Id']]['bid'] = @$value['badge_Id'];	
+							$badgesList[@$value['badge_Id']]['count'] +=1; 
+							$badgesList[@$value['badge_Id']]['name']=$value['style']; 						
+						}
+						$k++;
+					}
+				}		
+				$objPHPExcel->setActiveSheetIndex(0)
+					->setCellValue('A'.$row_count, date('m/d/Y',$order->order_date))
+					->setCellValue('B'.$row_count, str_pad($order->order_id,6,'0',STR_PAD_LEFT))
+					->setCellValue('C'.$row_count, $order->store_number)
+					->setCellValue('D'.$row_count, ($order->order_shipdate==0) ? 'pending' : date('m/d/Y',$order->order_shipdate))
+					->setCellValue('E'.$row_count, isset($badgeshipping['aor'])?$badgeshipping['aor']:$order->store_aor)
+					->setCellValue('F'.$row_count, isset($badgesList['4']) ? $badgesList['4']['count'] : "0" )
+					->setCellValue('G'.$row_count, isset($badgesList['6']) ? $badgesList['6']['count'] : "0" )
+					->setCellValue('H'.$row_count, isset($badgesList['5']) ? $badgesList['5']['count'] : "0" )
+					->setCellValue('I'.$row_count, isset($badgesList['10']) ? $badgesList['10']['count'] : "0" )
+					->setCellValue('J'.$row_count, isset($badgesList['11']) ? $badgesList['11']['count'] : "0" )
+					->setCellValue('K'.$row_count, $order->order_mf_qty)
+					->setCellValue('L'.$row_count, $order->order_pf_qty)
+					->setCellValue('M'.$row_count, $order->order_extender_qty)
+					->setCellValue('N'.$row_count, $order->order_bar_qty);
+					
+				$badges	= @unserialize($order->order_items); 
+				$serviceyear = (isset($badges['serviceyear']))?$badges['serviceyear']:Null;
+				
+				$k =0; 
+				$row_number  = 'O'; 			
+				for($i=0;$i<=65;)
+				{
+					if(!empty($serviceyear) && @$serviceyear[$k]['div_Id']==($i==0)?1:$i)
+					{
+				 		$data_str =  isset($serviceyear[$k]['qty'])?$serviceyear[$k]['qty']:0;							 
+					}else
+					{
+						$data_str = "0";
+					} 
+					
+					$objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.$row_count,$data_str);					
+					
+					$i= $i+5;
+					
+					$row_number++;
+					$k++;
+				}
+
+				$totalAmount = 0;
+				$totalserice = 0;								
+				if(isset($badges['badges'])){ 
+					foreach ($badges['badges'] as $key => $value) {  
+						$totalAmount += isset($value['badge_price'])?$value['badge_price']:DEFAULT_BADGES_PRICE;
+					}
+				}
+				if($totalAmount==0){ $totalAmount = $order->order_total*DEFAULT_BADGES_PRICE; }
+				if(!empty($serviceyear)){
+					foreach ($serviceyear as $key => $value) {  
+						$totalserice+=(int)$value['qty']; 
+					}
+				} 
+				
+				$data_str = number_format( $totalAmount + $totalserice*EACH_CHARM_PRICE + $order->order_extender_qty*ACCESSORIES_EXT_PRICE + $order->order_bar_qty*ACCESSORIES_BAR_PRICE + $order->order_mf_qty*EXTRA_MAGNETS_PRICE + $order->order_pf_qty*EXTRA_PINS_PRICE, 2);					
+				
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.$row_count,$data_str);
+				$row_number++;
+
+				$data_str = HANDLING_CHARGE_PER_ORDER;
+				
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.$row_count,$data_str);
+				$row_number++;
+				
+				$data_str = number_format($totalAmount + $totalserice*EACH_CHARM_PRICE + $order->order_extender_qty*ACCESSORIES_EXT_PRICE + $order->order_bar_qty*ACCESSORIES_BAR_PRICE + $order->order_mf_qty*EXTRA_MAGNETS_PRICE + $order->order_pf_qty*EXTRA_PINS_PRICE +HANDLING_CHARGE_PER_ORDER,2);
+				
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.$row_count,$data_str);
+				$row_number++;
+				
+				$data_str = number_format(number_format($order->order_total*BADGES_NET_PRICE + $totalserice*CHARMS_NET_PRICE + $order->order_extender_qty*ACCESSORIES_EXT_NET_PRICE + $order->order_bar_qty*ACCESSORIES_BAR_NET_PRICE + $order->order_mf_qty*EXTRA_MAGNETS_NET_PRICE + $order->order_pf_qty*EXTRA_PINS_NET_PRICE, 2)+3.00,2);
+				
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.$row_count,$data_str);
+				$row_number++;
+				
+				$data_str = (isset($order->tracking_number) && $order->tracking_number !='') ? '<a target="_blank" href="https://www.google.com/#q='.$order->tracking_number.'">Track Order</a>' : '';
+				
+				$objPHPExcel->setActiveSheetIndex(0)->setCellValue($row_number.$row_count,$data_str);												
+
+				$objPHPExcel->getActiveSheet()->getStyle("$row_count:$row_count")->applyFromArray($styleArray);	
+
+				$row_count++;
+				$i++;
+
+			}
+		}
+		else
+		{
+			$objPHPExcel->getActiveSheet()->getStyle('2:2')->applyFromArray($styleArray);				
+			$objPHPExcel->setActiveSheetIndex(0)->setCellValue('A2', 'No Order')	;		
+		}
+		
+		// Redirect output to a client’s web browser (Excel2007)
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header('Content-Disposition: attachment;filename="JCP-Output.xlsx"');
+		header('Cache-Control: max-age=0');
+
+		// If you're serving to IE 9, then the following may be needed
+		header('Cache-Control: max-age=1');
+
+		// If you're serving to IE over SSL, then the following may be needed
+		header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+		header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+		header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+		header ('Pragma: public'); // HTTP/1.0
+
+		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+		$objWriter->save('php://output');
+		exit;               
+	}
+
 }
